72b95a114b26b54f83f79e74c09dc00998ad1f38
atsushi-tago-0209 <0209at@gmail.com>
2020-03-25 20:30:37 +0900

    ビュー作成中

---
+++ app/javascript/channels/index.js
@@ -0,0 +1,14 @@
+// Load all the channels within this directory and all subdirectories.
+// Channel files must be named *_channel.js.
+
+const channels = require.context('.', true, /_channel\.js$/)
+channels.keys().forEach(channels)
+
+var headerHeight = $('.header').outerHeight();
+$('a[href^="#"]').click(function() {
+    var href= $(this).attr("href");
+    var target = $(href);
+    var position = target.offset().top - headerHeight;
+    $('body,html').stop().animate({scrollTop:position}, 500);   
+    return false;
+});
